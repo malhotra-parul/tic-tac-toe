1a24ec2442bc9c4aedae2a5b8cd65c896328907c
malhotra-parul <parulm.business@gmail.com>
2020-01-14 19:18:26 +0530

    added move as a key to ordered list of moves

--- src/index.js
+++ src/index.js
@@ -78,7 +78,7 @@ class Game extends React.Component{
             //step is the current element, move is the index of current element
             const desc =  move ? 'Go to move #' + move : 'Go to game start!';
             return(
-                <li>
+                <li key={move}>
                     <button onClick= {()=> {this.jumpTo(move)}} >{desc}</button>
                 </li>
             )
